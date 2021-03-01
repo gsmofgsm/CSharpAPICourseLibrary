0bff1fb36d574f70847f2bdf1656ac1ed5db72ce
Qing Ma <gsmofgsm@hotmail.com>
2021-03-01 23:19:14 +0100

    Validate patch upserting

--- CourseLibrary.API/Controllers/CoursesController.cs
+++ CourseLibrary.API/Controllers/CoursesController.cs
@@ -134,7 +134,13 @@ namespace CourseLibrary.API.Controllers
             if (courseForAuthorFromRepo == null)
             {
                 var courseDto = new CourseForUpdateDto();
-                patchDocument.ApplyTo(courseDto);
+                patchDocument.ApplyTo(courseDto, ModelState);
+
+                if (!TryValidateModel(courseDto))
+                {
+                    return ValidationProblem(ModelState);
+                }
+
                 var courseToAdd = _mapper.Map<Entities.Course>(courseDto);
                 courseToAdd.Id = courseId;
 
